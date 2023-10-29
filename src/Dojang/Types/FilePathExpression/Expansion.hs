4ff0f284228a1a4496441e8939295b892ee6f08c
Hong Minhee <hong@minhee.org>
2023-10-29 15:26:30 +0900

    ExpansionWarning

--- src/Dojang/Types/FilePathExpression/Expansion.hs
+++ src/Dojang/Types/FilePathExpression/Expansion.hs
@@ -1,14 +1,24 @@
-module Dojang.Types.FilePathExpression.Expansion (expandFilePath) where
+module Dojang.Types.FilePathExpression.Expansion
+  ( ExpansionWarning (..)
+  , expandFilePath
+  ) where
 
 import Dojang.Types.FilePathExpression
   ( EnvironmentVariable
   , FilePathExpression (..)
   )
 
-import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import System.OsPath (OsPath, pack, pathSeparator, unsafeFromChar, (</>))
 import System.OsString (OsString)
+
+
+-- | A set of warnings that can occur during expansion.
+newtype ExpansionWarning
+  = -- | The case where an environment variable that is referenced in a
+    -- 'Substitution' is not defined.
+    UndefinedEnvironmentVariable EnvironmentVariable
+  deriving (Eq, Show)
 
 
 -- | Expands a 'FilePathExpression' into an 'OsPath'.
@@ -20,34 +30,42 @@ expandFilePath
   -- ^ A function that can look up an environment variable.
   -> (Text -> m OsString)
   -- ^ A function that encodes a 'Text' into an 'OsString'.
-  -> m OsPath
-  -- ^ The expanded 'OsPath'.
-expandFilePath (BareComponent component) _ encode = encode component
-expandFilePath (Root Nothing) _ _ = return $ pack [pathSeparator]
+  -> m (OsPath, [ExpansionWarning])
+  -- ^ The expanded 'OsPath', along with a list of warnings that occurred during
+  -- expansion (if any).
+expandFilePath (BareComponent component) _ encode = do
+  f <- encode component
+  return (f, [])
+expandFilePath (Root Nothing) _ _ = return (pack [pathSeparator], [])
 expandFilePath (Root (Just driveLetter)) _ _ =
   return
-    $ pack
-      [ unsafeFromChar driveLetter
-      , unsafeFromChar ':'
-      , pathSeparator
-      ]
+    ( pack
+        [ unsafeFromChar driveLetter
+        , unsafeFromChar ':'
+        , pathSeparator
+        ]
+    , []
+    )
 expandFilePath (Concatenation expr1 expr2) lookupEnv encode = do
-  expanded1 <- expandFilePath expr1 lookupEnv encode
-  expanded2 <- expandFilePath expr2 lookupEnv encode
-  return $ expanded1 <> expanded2
+  (expanded1, ws1) <- expandFilePath expr1 lookupEnv encode
+  (expanded2, ws2) <- expandFilePath expr2 lookupEnv encode
+  return (expanded1 <> expanded2, ws1 ++ ws2)
 expandFilePath (PathSeparator expr1 expr2) lookupEnv encode = do
-  expanded1 <- expandFilePath expr1 lookupEnv encode
-  expanded2 <- expandFilePath expr2 lookupEnv encode
-  return $ expanded1 </> expanded2
-expandFilePath (Substitution envVar) lookupEnv _ =
-  fromMaybe mempty <$> lookupEnv envVar
+  (expanded1, ws1) <- expandFilePath expr1 lookupEnv encode
+  (expanded2, ws2) <- expandFilePath expr2 lookupEnv encode
+  return (expanded1 </> expanded2, ws1 ++ ws2)
+expandFilePath (Substitution envVar) lookupEnv _ = do
+  v <- lookupEnv envVar
+  case v of
+    Just v' -> return (v', [])
+    Nothing -> return (mempty, [UndefinedEnvironmentVariable envVar])
 expandFilePath (SubstitutionWithDefault envVar expr) lookupEnv encode = do
   env <- lookupEnv envVar
   case env of
-    Just env' | env' /= mempty -> return env'
+    Just env' | env' /= mempty -> return (env', [])
     _ -> expandFilePath expr lookupEnv encode
 expandFilePath (ConditionalSubstitution envVar expr) lookupEnv encode = do
   env <- lookupEnv envVar
   case env of
     Just env' | env' /= mempty -> expandFilePath expr lookupEnv encode
-    _ -> return mempty
+    _ -> return (mempty, [])
--- test/Dojang/Types/FilePathExpression/ExpansionSpec.hs
+++ test/Dojang/Types/FilePathExpression/ExpansionSpec.hs
@@ -4,7 +4,10 @@
 module Dojang.Types.FilePathExpression.ExpansionSpec (spec) where
 
 import Dojang.Types.FilePathExpression (FilePathExpression (..), (++), (+/+))
-import Dojang.Types.FilePathExpression.Expansion (expandFilePath)
+import Dojang.Types.FilePathExpression.Expansion
+  ( ExpansionWarning (..)
+  , expandFilePath
+  )
 
 import Data.Text (Text, unpack)
 import GHC.IO.Unsafe (unsafePerformIO)
@@ -29,93 +32,99 @@ spec = do
   describe "expandFilePath" $ do
     specify "BareComponent" $ do
       expanded <- expandFilePath (BareComponent "foo") undefined encode
-      expanded `shouldBe` os "foo"
+      expanded `shouldBe` (os "foo", [])
+
     specify "Root" $ do
       expanded <- expandFilePath (Root Nothing) undefined undefined
-      expanded `shouldBe` pack [pathSeparator]
+      expanded `shouldBe` (pack [pathSeparator], [])
       expanded' <- expandFilePath (Root $ Just 'C') undefined undefined
-      expanded' `shouldBe` (os "C:" <> pack [pathSeparator])
+      expanded' `shouldBe` (os "C:" <> pack [pathSeparator], [])
       expandedAbs <-
         expandFilePath
           (Root Nothing +/+ BareComponent "foo" +/+ BareComponent "bar")
           undefined
           encode
-      expandedAbs `shouldBe` pack [pathSeparator] </> os "foo" </> os "bar"
+      expandedAbs
+        `shouldBe` (pack [pathSeparator] </> os "foo" </> os "bar", [])
       expandedAbs' <-
         expandFilePath
           (Root (Just 'C') +/+ BareComponent "foo" +/+ BareComponent "bar")
           undefined
           encode
       expandedAbs'
-        `shouldBe` os "C:"
-        <> pack [pathSeparator]
-        </> os "foo"
-        </> os "bar"
+        `shouldBe` ( os "C:" <> pack [pathSeparator] </> os "foo" </> os "bar"
+                   , []
+                   )
+
     specify "Concatenation" $ do
       expanded <-
         expandFilePath
           (Substitution "FOO" ++ BareComponent "bar")
           (\e -> return $ if e == "FOO" then Just (os "baz") else Nothing)
           encode
-      expanded `shouldBe` os "baz" <> os "bar"
+      expanded `shouldBe` (os "baz" <> os "bar", [])
       expanded' <-
         expandFilePath
           (BareComponent "foo" ++ Substitution "BAR")
           (\e -> return $ if e == "BAR" then Just (os "qux") else Nothing)
           encode
-      expanded' `shouldBe` os "foo" <> os "qux"
+      expanded' `shouldBe` (os "foo" <> os "qux", [])
+
     specify "PathSeparator" $ do
       expanded <-
         expandFilePath
           (Substitution "FOO" +/+ BareComponent "bar")
           (\e -> return $ if e == "FOO" then Just (os "baz") else Nothing)
           encode
-      expanded `shouldBe` os "baz" </> os "bar"
+      expanded `shouldBe` (os "baz" </> os "bar", [])
       expanded' <-
         expandFilePath
           (BareComponent "foo" +/+ Substitution "BAR")
           (\e -> return $ if e == "BAR" then Just (os "qux") else Nothing)
           encode
-      expanded' `shouldBe` os "foo" </> os "qux"
+      expanded' `shouldBe` (os "foo" </> os "qux", [])
+
     specify "Substitution" $ do
       expanded <-
         expandFilePath
           (Substitution "FOO")
           (\e -> return $ if e == "FOO" then Just (os "bar") else Nothing)
           encode
-      expanded `shouldBe` os "bar"
+      expanded `shouldBe` (os "bar", [])
       notFound <-
         expandFilePath
           (Substitution "FOO")
           (const $ return Nothing)
           encode
-      notFound `shouldBe` mempty
+      notFound `shouldBe` (mempty, [UndefinedEnvironmentVariable "FOO"])
+
     specify "SubstitutionWithDefault" $ do
       expanded <-
         expandFilePath
           (SubstitutionWithDefault "FOO" (BareComponent "baz"))
           (\e -> return $ if e == "FOO" then Just (os "bar") else Nothing)
           encode
-      expanded `shouldBe` os "bar"
+      expanded `shouldBe` (os "bar", [])
       empty <-
         expandFilePath
           (SubstitutionWithDefault "FOO" (Substitution "BAR"))
           (\e -> return $ Just $ if e == "FOO" then mempty else os "qux")
           encode
-      empty `shouldBe` os "qux"
+      empty `shouldBe` (os "qux", [])
       notFound <-
         expandFilePath
           (SubstitutionWithDefault "FOO" (BareComponent "bar"))
           (\e -> return $ if e == "FOO" then Nothing else Just $ os "qux")
           encode
-      notFound `shouldBe` os "bar"
+      notFound `shouldBe` (os "bar", [])
+
     specify "ConditionalSubstitution" $ do
       expanded <-
         expandFilePath
           (ConditionalSubstitution "FOO" (BareComponent "baz"))
           (\e -> return $ if e == "FOO" then Just (os "bar") else Nothing)
           encode
-      expanded `shouldBe` os "baz"
+      expanded `shouldBe` (os "baz", [])
       expanded' <-
         expandFilePath
           (ConditionalSubstitution "FOO" (Substitution "BAZ"))
@@ -125,16 +134,16 @@ spec = do
               _ -> Nothing
           )
           encode
-      expanded' `shouldBe` os "qux"
+      expanded' `shouldBe` (os "qux", [])
       empty <-
         expandFilePath
           (ConditionalSubstitution "FOO" (BareComponent "baz"))
           (const $ return $ Just mempty)
           encode
-      empty `shouldBe` mempty
+      empty `shouldBe` (mempty, [])
       notFound <-
         expandFilePath
           (ConditionalSubstitution "FOO" (BareComponent "baz"))
           (const $ return Nothing)
           encode
-      notFound `shouldBe` mempty
+      notFound `shouldBe` (mempty, [])
